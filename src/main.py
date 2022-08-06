d6d8d8e569f5c26a96ff30cdc9170f6dd4cb1d45
yashahmad <yash.ahd786@gmail.com>
2022-08-06 20:49:00 +0530

    Fetch all badges

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def index():
-    return 'hello world'
+def get_db_connection():
+    conn = sqlite3.connect('db/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/badges',methods=['GET'])
+def listBadges():
+    conn = get_db_connection()
+    badges = conn.execute('SELECT * FROM badges').fetchall()
+    conn.close()
+    return render_template('badges.html', badges=badges)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
